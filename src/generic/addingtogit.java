e7c11a80df9115df60e84d22f701e9ad7ae22227
tyss <tyss@localhost>
2017-12-12 11:44:17 +0530

    adding new file to bigbasket

---
+++ src/generic/addingtogit.java
@@ -0,0 +1,14 @@
+package generic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class addingtogit
+{
+	@Test
+	public void message()
+	{
+		Reporter.log("hiiii",true);
+	}
+
+}
