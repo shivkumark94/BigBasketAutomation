5532cf2216d858019f03e27049f9e7e3256caf02
tyss <tyss@localhost>
2017-12-12 11:46:18 +0530

    adding one more to big basket

---
+++ src/generic/addingonemoretogit.java
@@ -0,0 +1,14 @@
+package generic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class addingonemoretogit 
+{
+	@Test
+	public void message()
+	{
+		Reporter.log("hiiii",true);
+	}
+
+}
